6a8e397a02d2b63697f8310de3079e2e51277583
DappioFds <dappiotrabalho@gmail.com>
2025-02-26 20:30:43 -0300

    Calcular Alguel Carro feito

---
+++ FixacaoConceitosBasicos1/Model/CalculaAluguel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixacaoConceitosBasicos1.Model
+{
+    public class CalculaAluguel
+    {
+        public static double Calcular(int dias, int quilometragemInicial, int quilometragemFinal)
+        {
+            double valorDiario = 95;
+            double valorPorKm = 0.35;
+            int quilometragemusada = quilometragemFinal - quilometragemInicial;
+
+            return (dias * valorDiario) + (quilometragemusada * valorPorKm);
+        }
+    }
+}
