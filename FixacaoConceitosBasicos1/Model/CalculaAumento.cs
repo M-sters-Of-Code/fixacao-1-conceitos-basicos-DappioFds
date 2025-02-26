da8e0932ea07650b2c72bb01c9b79fa475de4b91
DappioFds <dappiotrabalho@gmail.com>
2025-02-26 20:16:31 -0300

    Calcula aumento feito

---
+++ FixacaoConceitosBasicos1/Model/CalculaAumento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixacaoConceitosBasicos1.Model
+{
+    public class CalculaAumento
+    {
+        public static double aumento(double salario, double aumento)
+        {
+            return salario + aumento;
+        }
+
+    }
+}
