49e11431d7e75cefcbb6b36ddaf59c625d509157
DappioFds <dappiotrabalho@gmail.com>
2025-02-26 20:19:58 -0300

    Calcula Desconto feito

---
+++ FixacaoConceitosBasicos1/Model/CalculaDesconto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixacaoConceitosBasicos1.Model
+{
+    public class CalculaDesconto
+    {
+        public static double Desconto(double valor, double desconto)
+        {
+            return valor - (valor * desconto / 100);
+        }
+    }
+}
